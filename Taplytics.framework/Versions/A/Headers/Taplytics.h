91b76b3876dff36f552d16b55e5dabf036ed97b0
jonathannorris <jonathan@syrp.com>
2014-06-09 12:53:33 -0400

    updated SDK to 1.3.0

--- Taplytics.framework/Versions/A/Headers/Taplytics.h
+++ Taplytics.framework/Versions/A/Headers/Taplytics.h
@@ -6,7 +6,13 @@
 //
 
 #import <UIKit/UIKit.h>
-#import "TaplyticsManager.h"
+
+typedef enum {
+    TLDev,
+    TLProduction,
+    TLLocalHost,
+    TLLocalTest
+} TLServer;
 
 typedef void(^TLExperimentBlock)(NSDictionary *variables);
 
@@ -14,51 +20,57 @@ typedef void(^TLExperimentBlock)(NSDictionary *variables);
 
 @optional
 /** 
- Delegate method called when a experiment is changed, use this to call runCodeExperiment:withBaseline:variations: again
- in your code to test and visually see the different code experiments. Only neccisary for code experiments, visual experiments
+ Delegate method called when an experiment is changed, use this to call runCodeExperiment:withBaseline:variations: again
+ in your code to test and visually see the different code experiments. Only necessary for code experiments, visual experiments
  will update themselves.
- @param experimentName the name of the experiment
- @param variationName the name of the experiment varariation, nil if Baseline
+ @param experimentName The name of the experiment
+ @param variationName The name of the experiment variation, nil if Baseline
  */
 - (void)taplyticsExperimentChanged:(NSString*)experimentName variationName:(NSString*)variationName;
 
-- (void)taplyticsDeviceInfo:(NSDictionary*)deviceInfo;
-
-- (void)taplyticsLastApps:(NSArray*)lastApps;
-
 @end
+
 
 @interface Taplytics : NSObject
 
 /**
  Start the Taplytics SDK with your api key. the api key can be found in the 'project settings' page.
+ Console Logging: Taplytics will only log to the console in development builds.
  @param apiKey your api key
  */
 + (void)startTaplyticsAPIKey:(NSString*)apiKey;
 
 /**
  Start the Taplytics SDK with your api key. the api key can be found in the 'project settings' page.
- @param apiKey your api key
- @param options taplytics options dictionary, used for testing. Options include:
-            - @{"liveUpdate":@NO} to force production mode, or @YES to force live update mode for testing.
-            - @{"delayLoad":@2} allows Taplytics to show your launch image and load its configuration for a maximum number of seconds
-                on app startup. This is useful when running experiments on the first screen of your app, so users will get shown a variation 
-                on the first launch of your app.
+ Console Logging: Taplytics will only log to the console in development builds.
+ @param apiKey Your API key
+ @param options Taplytics options dictionary, used for testing. Options include:
+            - @{@"delayLoad":@2} allows Taplytics to show your app's launch image and load its configuration for a maximum number of seconds
+                on app launch. This is useful when running a code experiments on the first screen of your app, this will ensure that your users
+                will get shown a variation on the first launch of your app.
+            - @{@"liveUpdate":@NO} Taplytics will auto-detect an app store build or a development build. But to force production mode use @NO,
+                or @YES to force live update mode for testing.
+            - @{@"shakeMenu":@NO} To disable the Taplytics development mode shake menu set @NO, only use if you have your own development shake menu.
  */
- 
 + (void)startTaplyticsAPIKey:(NSString*)apiKey options:(NSDictionary*)options;
 
 /**
- Updates Taplytics configuration in a background fetch, only avaliable in iOS 7. It is HIGHLY recomended to implement background fetch
- in your apps, to allow Taplytics to have an upto date configuration on app launch.
- @param completionBlock completion block called when fetch is complete
+ Updates Taplytics configuration in a background fetch, only available in iOS 7. It is HIGHLY recommended to implement background fetch
+ in 'application:performFetchWithCompletionHandler:' in your UIApplicationDelegate, to allow Taplytics to update its configuration regularly.
+ For Example:
+ 
+ - (void)application:(UIApplication *)app performFetchWithCompletionHandler:(void (^)(UIBackgroundFetchResult))completeBlock {
+    [Taplytics performBackgroundFetch:completeBlock];
+ }
+ 
+ @param completionBlock Completion block called when fetch is complete
  */
 + (void)performBackgroundFetch:(void(^)(UIBackgroundFetchResult result))completionBlock NS_AVAILABLE_IOS(7_0);
 
 /**
  Optionally set the taplytics delegate when you need to know when a experiment has changed. For example if you are testing 
  a code experiment on your root view and want to visually see the different variations.
- @param delegate
+ @param delegate The delegate for the receiver. The delegate must implement the TaplyticsDelegate protocol.
  */
 + (void)setTaplyticsDelegate:(id<TaplyticsDelegate>)delegate;
 
@@ -77,14 +89,24 @@ typedef void(^TLExperimentBlock)(NSDictionary *variables);
  }, @"variation2": ^(NSDictionary *variables) {
  
  }}];
+ 
+ @param experimentName Name of the experiment to run
+ @param baselineBlock Baseline block called if experiment is in baseline variation
+ @param variationNamesAndBlocks NSDictionary with keys of variation names and values of variation blocks.
  */
 + (void)runCodeExperiment:(NSString*)experimentName withBaseline:(TLExperimentBlock)baselineBlock variations:(NSDictionary*)variationNamesAndBlocks;
 
 /**
- Report that an experiment goal has been achieved, optionally pass number value to track goal such as purchase revenue.
+ Report that an experiment goal has been achieved.
+ @param goalName the name of the experiment goal
  */
 + (void)goalAchieved:(NSString*)goalName;
 
+/**
+ Report that an experiment goal has been achieved, optionally pass number value to track goal such as purchase revenue.
+ @param goalName The name of the experiment goal
+ @param value A numerical value to be tracked with the goal. For example purcahse revenue.
+ */
 + (void)goalAchieved:(NSString*)goalName value:(NSNumber*)value;
 
 /**
@@ -92,8 +114,14 @@ typedef void(^TLExperimentBlock)(NSDictionary *variables);
  */
 + (void)startTaplyticsAPIKey:(NSString*)apiKey liveUpdate:(BOOL)liveUpdate __deprecated;
 
+/**
+ DEPRECATED Start Taplytics Methods, please use startTaplyticsAPIKey:options:
+ */
 + (void)startTaplyticsAPIKey:(NSString*)apiKey server:(TLServer)server __deprecated;
 
+/**
+ DEPRECATED Start Taplytics Methods, please use startTaplyticsAPIKey:options:
+ */
 + (void)startTaplyticsAPIKey:(NSString*)apiKey server:(TLServer)server liveUpdate:(BOOL)liveUpdate __deprecated;
 
 @end
