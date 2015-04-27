19a9abbc00064a61fd777c2d2a4741dcf7fade04
Jonathan Norris <jonathannorris@gmail.com>
2015-04-27 10:15:12 -0700

    updating to Taplytics 2.1.76

---
+++ Taplytics.framework/Headers/TaplyticsOptions.h
@@ -0,0 +1,23 @@
+//
+//  TaplyticsOptions.h
+//  Taplytics
+//
+//  Copyright (c) 2014 Taplytics All rights reserved.
+//
+
+extern NSString *const TaplyticsOptionShowLaunchImage;
+
+extern NSString *const TaplyticsOptionTrackLocation;
+extern NSString *const TaplyticsOptionTrackStoreKit;
+extern NSString *const TaplyticsOptionTrackSocialSignIn;
+extern NSString *const TaplyticsOptionTrackiAdInstallation;
+extern NSString *const TaplyticsOptionTrackPhotoLibraryAccess;
+
+extern NSString *const TaplyticsOptionSourceGoogleAnalytics;
+extern NSString *const TaplyticsOptionSourceFlurry;
+extern NSString *const TaplyticsOptionSourceMixpanel;
+extern NSString *const TaplyticsOptionSourceIntercom;
+extern NSString *const TaplyticsOptionSourceParse;
+extern NSString *const TaplyticsOptionSourceApsalar;
+extern NSString *const TaplyticsOptionSourceAdobe;
+extern NSString *const TaplyticsOptionSourceLocalytics;
